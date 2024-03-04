605bb621f25ed44e633b83c3e714cc7053c3a1ea
Elias Pehkonen <elias.pehkonen@metropolia.fi>
2024-03-04 13:39:19 +0200

    added Dockerfile and modified code and pom

--- src/main/java/FahrtToCel.java
+++ src/main/java/FahrtToCel.java
@@ -11,4 +11,17 @@ public class FahrtToCel {
     public static float kelvinToFahrt(float kel){
         return (float) ((kel-273.15) * (9/5) + 32);
     }
+
+    public static void main(String[] args) {
+        FahrtToCel ftc = new FahrtToCel();
+        System.out.println("32 F is " + ftc.convertFahrt(32) + " °C");
+        System.out.println("212 F is " + ftc.convertFahrt(212) + " °C");
+        System.out.println("98,6 F is " + ftc.convertFahrt(98.6f) + " °C");
+        System.out.println("68 F is " + ftc.convertFahrt(68) + " °C");
+        System.out.println("273,15 K is " + ftc.kelvinToCel(273.15f) + " °C");
+        System.out.println("373,15 K is " + ftc.kelvinToCel(373.15f) + " °C");
+        System.out.println("310,15 K is " + ftc.kelvinToCel(310.15f) + " °C");
+        System.out.println("293,15 K is " + ftc.kelvinToCel(293.15f) + " °C");
+        System.out.println("300,1 K is " + ftc.kelvinToFahrt(300.1F) + " F");
+    }
 }
